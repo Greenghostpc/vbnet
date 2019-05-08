abdd0ce851be971abaa787be212b2ef795c0a03e
Joseph Robinson <smartech56@outlook.com>
2019-05-08 11:35:23 -0700

    Added SharedClass Code

---
+++ SharedClass/SharedClass/ClassLibrary1/UserControl1.vb
@@ -0,0 +1,10 @@
+﻿Public Class UserControl1
+    Public Property caption As String
+    Public Property buttontext As String
+    Public Property bkcolor As System.Drawing.Color
+    Private Sub UserControl1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.text = caption
+        Button1.Text = buttontext
+        Me.BackColor = bkcolor
+    End Sub
+End Class
---
+++ SharedClass/SharedClass/SharedClass/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SharedClass")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("SharedClass")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1dea0dda-c1a2-4b33-bf02-2127a3859edf")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
